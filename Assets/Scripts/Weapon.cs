59484cfca1c822b28318308d7c4661e959050977
SuperSolga <supersolga.183@gmail.com>
2023-02-14 19:28:29 +0100

    Ajout du système de pistolet dans la hiérarchie Unity

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject[] loadout;
+    public Transform weaponParent;
+
+    private GameObject currentObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
+    }
+
+    void Equip(int index) 
+    { 
+        if(currentObject != null) Destroy(currentObject);
+
+        GameObject newEquipment = Instantiate(loadout[index], weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        newEquipment.transform.localPosition = Vector3.zero;
+        newEquipment.transform.localEulerAngles = Vector3.zero;
+
+        currentObject= newEquipment;
+    }
+}
